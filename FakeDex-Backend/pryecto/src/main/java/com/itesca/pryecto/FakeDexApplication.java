8e086b7b7cab1c53e3a7616b64e3e88a2aac4692
Luis Rodriguez <18130170@e-itesca.edu.mx>
2022-10-30 18:56:32 -0700

    v1.3 docker deployment

--- FakeDex-Backend/pryecto/src/main/java/com/itesca/pryecto/FakeDexApplication.java
+++ FakeDex-Backend/pryecto/src/main/java/com/itesca/pryecto/FakeDexApplication.java
@@ -1,21 +1,21 @@
-package com.itesca.demo;
+package com.itesca.pryecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class FakeDexApplication {
 
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(FakeDexApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello (@RequestParam(value = "name", defaultValue = "World") String name){
-		return String.format("Hello %s!", name);
-	}
 }
--- FakeDex-Backend/pryecto/src/test/java/com/itesca/pryecto/FakeDexApplicationTests.java
+++ FakeDex-Backend/pryecto/src/test/java/com/itesca/pryecto/FakeDexApplicationTests.java
@@ -1,4 +1,4 @@
-package com.itesca.demo;
+package com.itesca.pryecto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
